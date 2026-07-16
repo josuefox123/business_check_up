f8fcb5603dbdb0e9661216398a8e8dd3c5dd63e8
CCI Benin <contact@cci.bj>
2026-07-16 12:15:08 +0100

    fix: global scroll to top on route / tab change to avoid keeping previous scroll height

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import DiagnosticApp from './DiagnosticApp.jsx';
 import { AdminApp } from './components/admin/AdminApp.jsx';
+
+// Utility component to force scroll restoration to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/admin/*" element={<AdminApp />} />
         <Route path="/*" element={<DiagnosticApp />} />
